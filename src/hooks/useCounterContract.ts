849d248aa2d44a012b8c6af162da59f33a12b4ad
xiong.c <xiong@xiongcdeMacBook-Pro.local>
2023-12-14 11:31:11 +0800

    xiong/feature/tonApp-v1

---
+++ src/hooks/useCounterContract.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Counter } from '../contracts/Counter';
+import { useTonClient } from './useTonClient';
+import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
+import { Address, OpenedContract,toNano } from '@ton/core';
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse('EQDnRliuDE4gTk1xRUVQGN_nmmDjRykv5FkDFLz2djWkXZ0W') // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      setVal(null);
+      const val = await counterContract.getCounter();
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
+    }
+    getValue();
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrease(sender,{
+        increaseBy: 1,
+        value: toNano('0.0027'),
+    });
+    },
+  };
+}
